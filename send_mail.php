002d8bac7643797a45f7298e2d6ff5d93c95f887
pradeep824567 <118910452+Pradeeprdncas@users.noreply.github.com>
2026-02-14 06:30:43 +0530

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -19,8 +19,8 @@ try {
     $mail->Port       = 465;
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');         //Set the sender of the message (your email address)
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient (your bae's email address)
+    $mail->setFrom('achupradeep10012025@gmail.com', 'Mailer');         //Set the sender of the message (your email address)
+    $mail->addAddress('achupradeep10012025@gmail.com', 'Joe User');     //Add a recipient (your bae's email address)
 
     //Content
     $mail->isHTML(true);
